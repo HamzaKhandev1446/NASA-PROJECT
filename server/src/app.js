dd107e83cdbe6b8dc14b113c48b99d538b8f78df
Hamza Khan <hamza.khan@azimutlabs.com>
2023-08-20 21:49:50 +0500

    Central repository setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,24 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const cors = require("cors");
+const morgan = require('morgan')
 
 const planetsRouter = require("./routes/planets/planets.router");
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:8000",
   })
 );
+app.use(morgan('combined'))
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
